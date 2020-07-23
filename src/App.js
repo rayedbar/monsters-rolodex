a85f13e8dfc6affe0f635d76f7f7bebe1a8567a3
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-07-23 16:22:16 +0600

    feat: Fetch and display monsters

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import CardList from "./components/card-list/card-list.component";
+import "./App.css";
 
-function App() {
+const MONSTERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+const useFetchMonsters = () => {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    async function fetchMonsters() {
+      const response = await fetch(MONSTERS_URL);
+      const monsters = await response.json();
+
+      setMonsters(monsters);
+    }
+    fetchMonsters();
+  }, []);
+
+  return monsters;
+};
+
+const App = () => {
+  const monsters = useFetchMonsters();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList monsters={monsters} />
     </div>
   );
-}
+};
 
 export default App;
