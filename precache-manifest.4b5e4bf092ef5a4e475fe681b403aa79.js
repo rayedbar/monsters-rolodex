b7dc6ff50ed705d243e377aab2bd17202a4cc1dc
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-07-23 18:46:51 +0600

    Updates

---
+++ precache-manifest.4b5e4bf092ef5a4e475fe681b403aa79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5082780d2ab6088f1c89f53d284caa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "39a51275f5196adcc7ac",
+    "url": "/monsters-rolodex/static/css/main.87a21ccd.chunk.css"
+  },
+  {
+    "revision": "d5f77de2379ff389782c",
+    "url": "/monsters-rolodex/static/js/2.9c39fcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9c39fcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a51275f5196adcc7ac",
+    "url": "/monsters-rolodex/static/js/main.5d746156.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
